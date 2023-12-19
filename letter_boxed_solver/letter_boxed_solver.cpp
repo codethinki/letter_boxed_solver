7069d7c6cc6ad263f10c5dd59d1450aacff813ab
lukas thomann <kartoffel_lukas@outlook.de>
2023-12-19 23:27:56 +0100

    added some global variables to customize things and a PREPARE_WORD_LIST define

--- letter_boxed_solver/letter_boxed_solver.cpp
+++ letter_boxed_solver/letter_boxed_solver.cpp
@@ -9,13 +9,23 @@
 #include <unordered_map>
 #include <vector>
 
+#undef PREPARE_WORD_LIST
+
+constexpr uint32_t MIN_WORDS = 1;
+constexpr uint32_t MAX_WORDS = 3;
+constexpr uint32_t CHARS_PER_SIDE = 3;
+constexpr uint32_t SIDES = 4;
+constexpr uint32_t CHARS = SIDES * CHARS_PER_SIDE;
+
+namespace cth {
+
 using namespace std;
 
 vector<string> loadTextFile(const string_view path) {
 	vector<string> data{};
 	fstream file(path.data());
 	string line;
-	while (getline(file, line)) data.push_back(line);
+	while(getline(file, line)) data.push_back(line);
 
 	file.close();
 	return data;
@@ -24,52 +34,59 @@ template<typename RandIt>
 void writeTextFile(const string_view filepath, RandIt data_begin, const RandIt& data_end, const char delimiter = '\n') {
 	ofstream file(filepath.data(), ios::ate);
 
-	while (data_begin != data_end) {
+	while(data_begin != data_end) {
 		file << *data_begin << delimiter;
 		++data_begin;
 	}
 	file.close();
 }
-void prepareWordList(const string_view path) {
-	vector<string> content = loadTextFile(path);
+int countWords(const string_view filepath, const char list_delimiter = '\n') {
+	ifstream file(filepath.data());
+	return count(istreambuf_iterator<char>(file), istreambuf_iterator<char>(), list_delimiter);
+}
+void prepareWordList(const string_view filepath, const char list_delimiter = '\n') {
+	if(countWords(filepath, list_delimiter) < 20) {
+		cout << '\n' << "ERROR: undefine PREPARE_WORD_LIST, change delimiter or get a better list" << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	vector<string> content = loadTextFile(filepath);
 	vector<string> wordlist{};
 
 	vector<uint32_t> removable{};
 #pragma omp parallel for
-	for (int i = 0; i < content.size(); i++) {
+	for(int i = 0; i < content.size(); i++) {
 		string& line = content[i];
 		bool valid = line.size() >= 3;
 
-		for (int k = 0; valid && k < line.size(); k++) {
+		for(int k = 0; valid && k < line.size(); k++) {
 			char& c = line[k];
-			if (c >= 'a' && c <= 'z') continue;
-			if (c >= 'A' && c <= 'Z') {
+			if(c >= 'a' && c <= 'z') continue;
+			if(c >= 'A' && c <= 'Z') {
 				c += 'a' - 'A';
 				continue;
 			}
 			valid = false;
 		}
 #pragma omp critical
-		if (!valid) removable.push_back(i);
+		if(!valid) removable.push_back(i);
 	}
 	ranges::sort(removable);
-	for (int i = 0; i < removable.size(); i++) content.erase(content.begin() + removable[i] - i);
+	for(int i = 0; i < removable.size(); i++) content.erase(content.begin() + removable[i] - i);
 
 	ranges::sort(content);
 
-	writeTextFile(path, content.begin(), content.end(), ',');
+	writeTextFile(filepath, content.begin(), content.end(), ',');
 }
 
 
 
 vector<string> loadDictionaryFromWordlist(const string_view dictionary_path, const string_view valid_chars) {
-	constexpr static uint32_t ROW_SIZE = 3;
-
 	vector<string> words{};
 	ifstream file(dictionary_path.data());
 
-	if (!file.is_open()) {
-		cout << "ERROR: no dictionary found";
+	if(!file.is_open()) {
+		cout << "\nERROR: no dictionary found\n";
 		exit(EXIT_FAILURE);
 	}
 
@@ -79,17 +96,17 @@ vector<string> loadDictionaryFromWordlist(const string_view dictionary_path, con
 
 		string line;
 		vector<string> validWords{};
-		while (!finished) {
+		while(!finished) {
 #pragma omp critical
 			finished = !static_cast<bool>(getline(file, line, ','));
 
 			size_t prevSide = valid_chars.find(line[0]);
-			for (int i = 1; prevSide != string::npos && i < line.size(); i++) {
+			for(int i = 1; prevSide != string::npos && i < line.size(); i++) {
 				const size_t side = valid_chars.find(line[i]);
 
-				prevSide = (prevSide / ROW_SIZE == side / ROW_SIZE) ? string::npos : side;
+				prevSide = (prevSide / CHARS_PER_SIDE == side / CHARS_PER_SIDE) ? string::npos : side;
 			}
-			if (prevSide == string::npos) continue;
+			if(prevSide == string::npos) continue;
 
 			validWords.push_back(line);
 		}
@@ -103,39 +120,72 @@ vector<string> loadDictionaryFromWordlist(const string_view dictionary_path, con
 
 	return words;
 }
-
 /**
  * \brief increments the correct offset value for the next word
- * \param offsets offset in validWords list
- * \return index of the offset that needs to be updated .> offsets.size() = no update required
+ * \param offsets offset, offset_max in validWords list
+ * \return index of the offset that needs to be updated -> offsets.size() = no update required
  */
 inline uint32_t nextOffset(vector<pair<uint32_t, uint32_t>>& offsets) {
 	//offset[0] is fixed
-	for (int i = offsets.size() - 1; i >= 1; --i) {
+	for(int i = offsets.size() - 1; i >= 1; --i) {
 		offsets[i].first += 1;
-		if (offsets[i].first < offsets[i].second) return i + 1;
+		if(offsets[i].first < offsets[i].second) return i + 1;
 	}
 	return 0;
 }
-inline void nextOffsetSet(const int first_update, vector<pair<uint32_t, uint32_t>>& offsets, const vector<string_view>& dictionary,
-	unordered_map<char, uint32_t> char_map, const array<uint32_t, 13>& first_char_map) {
+inline void nextOffsetSet(const int first_update, vector<pair<uint32_t, uint32_t>>& offsets,
+	unordered_map<char, uint32_t> char_entry_map, const array<uint32_t, CHARS + 1>& dictionary_entries, const vector<string_view>& dictionary) {
 	assert(first_update > 0 && "first update cannot be < 1");
-	for (int i = first_update; i < offsets.size(); i++) {
-		const int lastCharIndex = char_map[dictionary[offsets[i - 1].first].back()];
-		offsets[i].first = first_char_map[lastCharIndex];
-		offsets[i].second = first_char_map[lastCharIndex + 1];
+	for(int i = first_update; i < offsets.size(); i++) {
+		const int lastCharIndex = char_entry_map[dictionary[offsets[i - 1].first].back()];
+		offsets[i].first = dictionary_entries[lastCharIndex];
+		offsets[i].second = dictionary_entries[lastCharIndex + 1];
 	}
 
 }
 
-vector<string> calcFixedSizeSolutions(const int word_count, const string_view sorted_valid_chars, const vector<string_view>& dictionary) {
+vector<string> calcFixedSizeSolutions(const int word_count, const string_view sorted_valid_chars, const unordered_map<char, uint32_t>& char_entry_map,
+	const array<uint32_t, CHARS + 1>& dictionary_entries, const vector<string_view>& dictionary) {
 	vector<string> solutions{};
 
-	unordered_map<char, uint32_t> charMap{};
-	for (int i = 0; i < sorted_valid_chars.size(); i++) charMap[sorted_valid_chars[i]] = i;
+#pragma omp parallel for shared(char_entry_map, dictionary_entries)
+	for(int i = 0; i < dictionary.size(); i++) {
+		vector<pair<uint32_t, uint32_t>> offsets(word_count);
+		//set the first offset based on the current fixed word
+		offsets[0].first = i;
+		offsets[0].second = dictionary.size();
 
-	const array<uint32_t, 13> firstCharMap = [sorted_valid_chars, dictionary]() {
-		array<uint32_t, 13> arr{};
+		int firstSetUpdate = 1;
+		while(firstSetUpdate) {
+			nextOffsetSet(firstSetUpdate, offsets, char_entry_map, dictionary_entries, dictionary);
+
+			string solution = "";
+			for(int k = 0; k < offsets.size(); k++) solution += dictionary[offsets[k].first];
+			bool valid = true;
+			for(int k = 0; valid && k < sorted_valid_chars.size(); k++) valid = solution.contains(sorted_valid_chars[k]);
+
+			if(valid) {
+				solution = "";
+				for(int k = 0; k < offsets.size(); k++) {
+					solution += dictionary[offsets[k].first];
+					solution += ", ";
+				}
+#pragma omp critical
+				solutions.push_back(solution.substr(0, solution.size() - 2));
+			}
+
+			firstSetUpdate = nextOffset(offsets);
+		}
+	}
+	return solutions;
+}
+
+vector<string> calcBestSolutions(const string_view sorted_valid_chars, const vector<string_view>& dictionary) {
+	vector<string> solutions{};
+	unordered_map<char, uint32_t> charEntryMap{};
+	for (int i = 0; i < sorted_valid_chars.size(); i++) charEntryMap[sorted_valid_chars[i]] = i;
+	const array<uint32_t, CHARS + 1> dictionaryEntries = [sorted_valid_chars, dictionary]() {
+		array<uint32_t, SIDES* CHARS_PER_SIDE + 1> arr{};
 		for (int i = 0; i < sorted_valid_chars.size(); i++) {
 			int k = 0;
 			for (; k < dictionary.size() && dictionary[k][0] != sorted_valid_chars[i]; k++);
@@ -148,96 +198,70 @@ vector<string> calcFixedSizeSolutions(const int word_count, const string_view so
 		return arr;
 		}();
 
-#pragma omp parallel for shared(charMap, firstCharMap)
-		for (int i = 0; i < dictionary.size(); i++) {
-			vector<pair<uint32_t, uint32_t>> offsets(word_count);
-			//set the first offset based on the current fixed word
-			offsets[0].first = i;
-			offsets[0].second = dictionary.size();
 
-			int firstSetUpdate = 1;
-			while (firstSetUpdate) {
-				nextOffsetSet(firstSetUpdate, offsets, dictionary, charMap, firstCharMap);
-
-				string solution = "";
-				for (int k = 0; k < offsets.size(); k++) solution += dictionary[offsets[k].first];
-				bool valid = true;
-				for (int k = 0; valid && k < sorted_valid_chars.size(); k++) valid = solution.contains(sorted_valid_chars[k]);
-
-				if (valid) {
-					solution = "";
-					for (int k = 0; k < offsets.size(); k++) {
-						solution += dictionary[offsets[k].first];
-						solution += ", ";
-					}
-#pragma omp critical
-					solutions.push_back(solution.substr(0, solution.size() - 2));
-				}
-
-				firstSetUpdate = nextOffset(offsets);
-			}
-		}
-		return solutions;
-}
-
-vector<string> calcBestSolutions(const string_view sorted_valid_chars, const vector<string_view>& dictionary_view) {
-	vector<string> solutions{};
-	for (int i = 1; solutions.empty() && i < 6; i++) solutions = calcFixedSizeSolutions(i, sorted_valid_chars, dictionary_view);
+	for(int i = MIN_WORDS; solutions.empty() && i < MAX_WORDS; i++) solutions = calcFixedSizeSolutions(i, sorted_valid_chars, charEntryMap, dictionaryEntries, dictionary);
 
 	ranges::sort(solutions, [](const string_view a, const string_view b) { return a.size() > b.size(); });
 
-	if (solutions.size() > 30) solutions.resize(30);
+	if(solutions.size() > 30) solutions.resize(30);
 
 	return solutions;
 }
 
 
 
-string getValidChars() {
+string getLetterBoxedSides() {
 	string validChars{};
 
 	cout << "input the characters of one rectangle each (lowercase):\n";
-	for (int i = 0; i < 4; i++) {
+	for(int i = 0; i < SIDES; i++) {
 		cout << "side [" << to_string(i + 1) << "]: ";
 		bool valid = true;
 		string input;
 		do {
-			if (!valid) cout << "  invalid, try again: ";
+			if(!valid) cout << "  invalid, try again: ";
 			cin >> input;
-			valid = input.size() == 3;
-			for (int k = 0; k < 3 && valid; k++) valid = input[k] >= 'a' && input[k] <= 'z' && !validChars.contains(input[k]);
-		} while (!valid);
+			valid = input.size() == CHARS_PER_SIDE;
+			for(int k = 0; k < CHARS_PER_SIDE && valid; k++) valid = input[k] >= 'a' && input[k] <= 'z' && !validChars.contains(input[k]);
+		} while(!valid);
 		validChars += input;
 	}
 
 	return validChars;
 }
 
+}
+
+
 int main() {
+	using namespace std;
+	using namespace cth;
+
 	const string wordlistPath = "words_hard.txt";
 
-	//prepareWordList(wordlistPath);
+#ifdef PREPARE_WORD_LIST
+	prepareWordList(wordlistPath);
+#endif
 
-	string validChars = getValidChars();
+	string letterBoxedSides = getLetterBoxedSides();
 
 	const auto start = chrono::high_resolution_clock::now();
 
-	auto dictionary = loadDictionaryFromWordlist(wordlistPath, validChars);
+	auto dictionary = loadDictionaryFromWordlist(wordlistPath, letterBoxedSides);
 
-
-
-	ranges::sort(validChars.begin(), validChars.end());
+	ranges::sort(letterBoxedSides);
 
 	vector<string_view> dictionaryView{};
 	dictionaryView.insert(dictionaryView.begin(), dictionary.begin(), dictionary.end());
 
-	const vector<string> solutions = calcBestSolutions(validChars, dictionaryView);
+	const vector<string> solutions = calcBestSolutions(letterBoxedSides, dictionaryView);
 
 	const float time = chrono::duration<float>(chrono::high_resolution_clock::now() - start).count();
 
-	for (int i = 1; i < solutions.size(); i++) cout << solutions[i] << '\n';
+	cout << '\n';
+	for(int i = 1; i < solutions.size(); i++) cout << solutions[i] << '\n';
 
-	cout << "\nbest solution: " << solutions.back() << '\n' << "took " << time << "s";
+	cout << "\nbest solution: " << solutions.back() << '\n' << "\ntook " << time << "s";
 
 	return EXIT_SUCCESS;
 }
